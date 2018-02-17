7144e41e81634d369130975a12269fa9c28ca931
Simon <sima@chetverukhin.ru>
2018-02-17 19:14:56 +0300

    feat(*): some changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "react-dom";
+import { render } from "react-snapshot";
 import { Provider } from "react-redux";
 import store from "./store";
 import "./index.css";
